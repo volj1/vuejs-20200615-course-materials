496be38a19d85d8ef5400d95b7eb4acdd98406fc
Grigorii K. Shartsev <me@shgk.me>
2020-06-18 19:47:54 +0500

    Add 01 final

--- 01-Welcome/02-Vue/script.js
+++ 01-Welcome/02-Vue/script.js
@@ -1,1 +1,75 @@
 import Vue from '/vendor/vue.esm.browser.js';
+import meetups from '/data/meetups-data.js';
+
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    meetups: [],
+    filter: {
+      date: '',
+      participation: '',
+      search: '',
+      view: '',
+    },
+  },
+
+  async mounted() {
+    this.meetups = await this.fetchMeetups();
+  },
+
+  computed: {
+    processedMeetups() {
+      return this.meetups.map((meetup) => ({
+        ...meetup,
+        cover: meetup.imageId ? `https://course-vue.javascript.ru/api/images/${meetup.imageId}` : undefined,
+        date: new Date(meetup.date),
+        localeDate: new Date(meetup.date).toLocaleString(navigator.language, {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        }),
+      }));
+    },
+
+    filteredMeetups() {
+      let filteredMeetups = this.processedMeetups;
+
+      if (this.filter.date === 'past') {
+        filteredMeetups = filteredMeetups.filter((meetup) => new Date(meetup.date) <= new Date());
+      } else if (this.filter.date === 'future') {
+        filteredMeetups = filteredMeetups.filter((meetup) => new Date(meetup.date) > new Date());
+      }
+
+      if (this.filter.participation === 'organizing') {
+        filteredMeetups = filteredMeetups.filter((meetup) => meetup.organizing);
+      } else if (this.filter.participation === 'attending') {
+        filteredMeetups = filteredMeetups.filter((meetup) => meetup.attending);
+      }
+
+      if (this.filter.search) {
+        const concatMeetupText = (meetup) =>
+          [meetup.title, meetup.description, meetup.place, meetup.organizer].join(' ').toLowerCase();
+        filteredMeetups = filteredMeetups.filter((meetup) =>
+          concatMeetupText(meetup).includes(this.filter.search.toLowerCase()),
+        );
+      }
+
+      return filteredMeetups;
+    },
+  },
+
+  methods: {
+    async fetchMeetups() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(meetups);
+        }, 1000);
+      });
+    },
+
+
+  }
+
+
+});
