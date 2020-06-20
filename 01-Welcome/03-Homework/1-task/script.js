0b57a366c698081d377b2adbed0edc9abf78bee2
Vladimir Lyutetskiy <vl@webinnovations.ru>
2020-06-20 16:08:09 +0300

    01-01-task

--- 01-Welcome/03-Homework/1-task/script.js
+++ 01-Welcome/03-Homework/1-task/script.js
@@ -1,4 +1,13 @@
 import Vue from '/vendor/vue.esm.browser.js';
 
-// const app = ...
-// Рекомендуется использовать МЕТОД в качестве обработчика события
+const app = new Vue({
+    el: "#app",
+    data: {
+        counter: 0
+    },
+    methods: {
+        increment() {
+            this.counter += 1;
+        }
+    }
+});
